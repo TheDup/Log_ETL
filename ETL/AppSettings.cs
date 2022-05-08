b7e5b89e537918fef79de3ed94f1935df869611e
DeVilliers(Div) <devilliers.duplessis@gmail.com>
2022-05-08 22:26:10 +0200

    Add project files.

---
+++ ETL/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace ETL
+{
+    public class AppSettings
+    {
+        public MariaDBSettings {get; set;}
+    }
+    public class MariaDBSettings
+    {
+        Server = "YOUR-SERVER.mysql.database.azure.com",
+                    Database = "YOUR-DATABASE",
+                    UserID = "USER@YOUR-SERVER",
+                    Password = "PASSWORD",
+                    SslMode = MySqlSslMode.Required,
+    }
+}
---
+++ Infrastructure/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace Infrastructure
+{
+    public class MySqlCreate
+    {
+        static async Task BuildDB()
+        {
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "YOUR-SERVER.mysql.database.azure.com",
+                Database = "YOUR-DATABASE",
+                UserID = "USER@YOUR-SERVER",
+                Password = "PASSWORD",
+                SslMode = MySqlSslMode.Required,
+            };
+
+        }
+    }
+}
